ac58d937d06af8a35c7e0e9e73616818ad8e45bd
Ozaky <bruna_ozaki@hotmail.com>
2025-07-31 00:10:56 -0300

    Adicionado Contador.java

---
+++ Controle de Fluxo/Contador.java
@@ -0,0 +1,13 @@
+public class Contador {
+    public static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+        if (parametroUm > parametroDois) {
+            throw new ParametrosInvalidosException();
+        }
+
+        int contagem = parametroDois - parametroUm;
+
+        for (int i = 1; i <= contagem; i++) {
+            System.out.println("Imprimindo o número " + i);
+        }
+    }
+}
