e72c30156a43f29dc20cd402ad986f5319fdb94c
Ozaky <bruna_ozaki@hotmail.com>
2025-08-12 01:06:30 -0300

    Adicionado Banco.java

---
+++ Banco Digital/Banco.java
@@ -0,0 +1,15 @@
+import java.util.List;
+
+public class Banco {
+    private String nome;
+    private List<Conta> contas;
+
+    public Banco(String nome, List<Conta> contas) {
+        this.nome = nome;
+        this.contas = contas;
+    }
+
+    public List<Conta> getContas() {
+        return contas;
+    }
+}
